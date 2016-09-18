13bdf2da4d08e37e06bc3436d77ba597036e5516
Alexei Matveev <alexei.matveev@gmail.com>
2016-09-19 00:42:30 +0200

    AM: split off refresh code

--- src/proto_zabbix/agent.clj
+++ src/proto_zabbix/agent.clj
@@ -30,44 +30,61 @@
 ;;  "clock" 1474141031,
 ;;  "ns" 670248125},
 ;;
-(defn- refresh-active-checks
+(defn- request-active-checks!
+  "Returns server response, or fails."
+  [server-host server-port]
+  (with-open [sock (Socket. server-host server-port)]
+    (p/send-recv sock
+                 {"request" "active checks",
+                  "host" "host.example.com",
+                  "host_metadata" "Proto-Zabbix Agent"})))
+
+(defn- get-active-checks!
   "Returns server response, blocks until success."
   [server-host server-port]
-  (let [ask #(with-open [sock (Socket. server-host server-port)]
-               (p/send-recv sock
-                            {"request" "active checks",
-                             "host" "host.example.com",
-                             "host_metadata" "Proto-Zabbix Agent"}))]
-    (loop []
-      (println "Asking for items ...")
-      (or (try
-            (let [res (ask)]
-              (and (= "success" (get res "response"))
-                   res))
-            (catch Exception e nil))
-          ;; There is no point to proceed for the active agent if the
-          ;; server is unresponsive. Retry after some interval:
-          (do
-            (Thread/sleep (* 10 1000))
-            (recur))))))
+  (loop []
+    (println "Asking for items ...")
+    (or (try
+          (let [res (request-active-checks! server-host server-port)]
+            (and (= "success" (get res "response"))
+                 res))
+          (catch Exception e nil))
+        ;; There is no point to proceed for the active agent if the
+        ;; server is unresponsive. Retry after some interval:
+        (do
+          (Thread/sleep (* 10 1000))
+          (recur)))))
+
+;; Initialize checks with the timestamp of the last report. If that
+;; didnt happen yet, e.g. the input "checks" is an empty list, use the
+;; beginning of the epoch:
+(defn- refresh-checks [checks response]
+  (for [c (get response "data")]
+    (let [key (get c "key")
+          ;; Checks are in a list, would like a guarantee the key is
+          ;; unique there:
+          last-value (reduce (fn [t c]
+                               (if-not (= key (get c "key"))
+                                 t
+                                 (max t (:last-value c))))
+                             0          ; epoch
+                             checks)]
+      (assoc c :last-value last-value))))
 
 (defn zabbix-agent-active
   "Emulates behaviour of an active Zabbix agent"
   [server-host server-port]
   (let [refresh-interval (* 1000 30)
-        last-refresh (System/currentTimeMillis)
-        response (refresh-active-checks server-host server-port)
-        _ (prn response)
-        ;; Initialize checks with the timestamp of the last
-        ;; report. That didnt yet happen, but pretend it did at the
-        ;; beginning of the epoch:
-        checks (for [c (get response "data")]
-                 (assoc c :last-value 0))]
+        refresh! (fn [checks]
+                   (let [response (get-active-checks! server-host server-port)]
+                     (prn response)
+                     (refresh-checks checks response)))]
     ;; Outer refresh loop. Ask the server for the items to be
     ;; delivered. Then spend some time regularly delivering the agent
-    ;; data before asking again:
-    (loop [last-refresh last-refresh
-           checks checks]
+    ;; data before asking again.  Supply an empty list of checks as
+    ;; input of the initial refresh:
+    (loop [last-refresh (System/currentTimeMillis)
+           checks (refresh! [])]
       ;; Inner loop. Spend refresh-interval sending agent data to the
       ;; server.  Send outdated items, then go to sleep for some
       ;; quantum of time to wake up again and check if any further
@@ -96,23 +113,13 @@
                          (Thread/sleep 1000)
                          (recur (System/currentTimeMillis)
                                 (concat check-now check-later)))))]
-        ;; Try refreshing the list of items again. FIXME: server
-        ;; list is authoritative, need taking timestamps from the
-        ;; local data:
-        (let [current-time (System/currentTimeMillis)
-              response (refresh-active-checks server-host server-port)
-              _ (prn response)
-              checks (for [c (get response "data")]
-                       (let [key (get c "key")
-                             ;; Checks are in a list, would like a
-                             ;; guarantee the key is unique there:
-                             last-value (apply max
-                                               (map :last-value
-                                                    (filter (fn [x] (= key (get x "key")))
-                                                            checks)))]
-                         (assoc c :last-value last-value)))]
-          (recur current-time
-                 checks))))))
+        ;; Try refreshing the list of items again. FIXME: server list
+        ;; is authoritative, need taking timestamps from the local
+        ;; data --- supply the current info on the check as input too:
+        (recur (System/currentTimeMillis)
+               (refresh! checks))))))
+
+
 
 ;; (zabbix-agent-active "localhost" 10051)
 
