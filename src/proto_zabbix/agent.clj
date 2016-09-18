abe8ac219f05b137a9b3a27788a2a4720060ad19
Alexei Matveev <alexei.matveev@gmail.com>
2016-09-18 21:37:19 +0200

    AM: lay off refresh-active-checks

--- src/proto_zabbix/agent.clj
+++ src/proto_zabbix/agent.clj
@@ -3,7 +3,7 @@
   (:import [java.net Socket]))
 
 ;;
-;; See also Java implementation in Zabbi/J [1]
+;; See also Java implementation in Zabbix/J [1]
 ;;
 ;; [1] https://bitbucket.org/mquigley/zabbixj
 ;;
@@ -16,7 +16,7 @@
 ;;
 ;; NOTE: Serializing JSON would also convert keywords such as :request
 ;; to strings but we are not  using that so far because vanilla Zabbix
-;; appears to use weired keys.
+;; appears to use weired keys occasionally.
 ;;
 ;; Then regularly send agent data in this form (not implemented)
 ;;
@@ -30,35 +30,47 @@
 ;;  "clock" 1474141031,
 ;;  "ns" 670248125},
 ;;
+(defn- refresh-active-checks
+  "Returns server response or nil on failure."
+  [server-host server-port]
+  (println "Asking for items ...")
+  (try
+    (with-open [sock (Socket. server-host server-port)]
+      (proto/send-recv sock
+                       {"request" "active checks",
+                        "host" "host.example.com",
+                        "host_metadata" "Proto-Zabbix Agent"}))
+    ;; Return nil on failure: (get nil "repsonse") will be nil as
+    ;; well:
+    (catch Exception e nil)))
+
 (defn zabbix-agent-active
   "Emulates behaviour of an active Zabbix agent"
   [server-host server-port]
-  (let [refresh-interval (* 1000 10)    ; 10 seconds
-        refresh #(with-open [sock (Socket. server-host server-port)]
-                   (println "Asking for items ...")
-                   (proto/send-recv sock
-                                    {"request" "active checks",
-                                     "host" "host.example.com",
-                                     "host_metadata" "Proto-Zabbix Agent"}))]
+  (let [refresh-interval (* 1000 10)]   ; 10 seconds
     ;; Outer refresh loop. Ask the server for the items to be
-    ;; delivered. The spend some time regularly delivering the items
+    ;; delivered. Then spend some time regularly delivering the items
     ;; before asking again:
     (loop [start-time (System/currentTimeMillis)
-           response (refresh)]
-      (when (= "success" (get response "response"))
-        ;; Inner loop. Send the items then go to sleep for some
-        ;; quantum of time to wake up again and check if any further
-        ;; action is required. Exit the loop to refresh the item list
-        ;; again:
-        (prn response)
+           response (refresh-active-checks server-host server-port)]
+      (prn response)
+      (if-not (= "success" (get response "response"))
+        ;; If the server did not deliver, wait before retrying:
+        (Thread/sleep (* 1000 20))
+        ;; Otherwise spend refresh-interval sending agent data to the
+        ;; server.  Inner loop. Send the items, then go to sleep for
+        ;; some quantum of time to wake up again and check if any
+        ;; further action is required. Exit the loop to refresh the
+        ;; item list again:
         (loop [current-time start-time]
           (when (>= refresh-interval (- current-time start-time))
             (let [items (get response "data")]
-              (prn "."))
+              (prn items))
             (Thread/sleep 1000)
-            (recur (System/currentTimeMillis))))
-        (recur (System/currentTimeMillis)
-               (refresh))))))
+            (recur (System/currentTimeMillis)))))
+      ;; Try refreshing the list of items again:
+      (recur (System/currentTimeMillis)
+             (refresh-active-checks server-host server-port)))))
 
 ;; (zabbix-agent-active "localhost" 10051)
 
