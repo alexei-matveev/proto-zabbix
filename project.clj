0ab1041c11eaeaf16bd21e400579ee78b88f87d5
Alexei Matveev <alexei.matveev@gmail.com>
2018-07-06 19:43:03 +0200

    AM: cheshire 5.8.0 and aut for uberjar

--- project.clj
+++ project.clj
@@ -4,11 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [cheshire "5.6.1"]]
+                 [cheshire "5.8.0"]]
   ;; lein run will start an emulator of a Zabbix server:
-  :main proto-zabbix.server             ; why ^:skip-aot?
-  :aot :all
-  ;; :profiles {:uberjar {:aot :all}}
+  :main proto-zabbix.server
+  :profiles {:uberjar {:aot :all}}
   ;;
   ;; Emit  warnings on  all reflection  calls.  The  native-image from
   ;; GraalVM toolbox, for example, will refuse to compile reflection:
