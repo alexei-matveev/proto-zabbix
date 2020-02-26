e4f0a95f005c44e2c431bfd49d1ef9ef41a7d706
Alexei Matveev <alexei.matveev@gmail.com>
2020-02-26 16:20:30 +0100

    AM: bump versions

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject f0bec0d/proto-zabbix "0.1.1-SNAPSHOT"
+(defproject f0bec0d/proto-zabbix "0.2.0-SNAPSHOT"
   :description "Zabbix Protocols in Clojure"
   :url "https://github.com/alexei-matveev/proto-zabbix"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [cheshire "5.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [cheshire "5.10.0"]]
   ;; lein run will start an emulator of a Zabbix server:
   :main proto-zabbix.server
   :profiles {:uberjar {:aot :all}}
